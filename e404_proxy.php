b9e60cad58c812f1fe0eaf4ffdd1fb827ba777d9
Damiano Seno <dax86it@gmail.com>
2013-01-27 21:14:13 +0100

    cleaned code

--- e404_proxy.php
+++ e404_proxy.php
@@ -1,42 +1,70 @@
 <?php
-// A http 404 response
-function do_404() {
+
+/**
+ * A http 404 response.
+ * */
+function do_404($message) {
   header('HTTP/1.0 404 Not Found');
+  header('X-E404:' . $message);
+  print 'E404: ' . $message;
   die();
 }
 
-// Gets configuration parameter
+/**
+ * Gets configuration parameter.
+ * */
 function cfg($key, $default = NULL) {
   return isset($_SERVER[$key]) ? $_SERVER[$key] : $default;
 }
 
-// Dest is mandatory
-if (!$dest = cfg('E404_PROXY_DEST')) {
-  print 'E404: no destination set';
-  do_404();
+/**
+ * Downloads remote file.
+ * */
+function wget($location) {
+  if (extension_loaded('curl')) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $location);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+  }
+  else {
+    $data = file_get_contents($location);
+  }
+
+  return $data;
 }
 
-// Test for filter, if provided
+// Destination is mandatory.
+if (!$dest = cfg('E404_PROXY_DEST')) {
+  do_404('no destination set');
+}
+
+// Test for filter, if provided.
 $uri = $_SERVER['REQUEST_URI'];
 if (($filter = cfg('E404_PROXY_FILTER')) && !preg_match($filter, $uri)) {
-  print 'E404: filter does not match, ' . $filter;
-  do_404();
+  do_404('filter does not match, ' . $filter);
 }
 
-// Adjust uri
-$dir = dirname($_SERVER['SCRIPT_NAME']);
-$uri = preg_replace('~^' . $dir . '~', '', $uri);
+// Dev version is in sub-path? Remove this path when resolving the name of file in production.
+// If production version is in sub-path you just need to append it to E404_PROXY_DEST.
+if ($path = cfg('E404_PROXY_PATH')) {
+  $uri = preg_replace('~^' . rtrim($path, '/') . '~', '', $uri);
+}
 
 $location = rtrim($dest, '/') . $uri;
 
-// Download to local, if asked
-if ($download = cfg('E404_PROXY_DOWNLOAD')) {
-  // TODO: add curl support
-  $contents = file_get_contents($location);
+// Download to local, if asked.
+if ($download = cfg('E404_PROXY_DL')) {
+  // Manual download, instead of using native copy().
+  // because it's easier to deal with proxies.
+  $contents = wget($location);
 
   if (!$contents) {
-    print 'E404: file is empty ' . $location;
-    do_404();
+    do_404('file is empty ' . $location);
   }
 
   $pwd = dirname(__FILE__);
@@ -44,21 +72,25 @@ if ($download = cfg('E404_PROXY_DOWNLOAD')) {
   $file = $pwd . $path;
   $dir = dirname($file);
 
+  // Permissions to new directories and downloaded files.
+  $perms = cfg('E404_PROXY_DL_PERMS', 0777);
+  $perms = intval($perms, 8);
+
   if (!is_dir($dir)) {
-    if (!mkdir($dir, 0777, TRUE)) {
-      print 'E404: unable to create directory ' . $dir;
-      do_404();
+    if (!mkdir($dir, $perms, TRUE)) {
+      do_404('unable to create directory ' . $dir);
     }
   }
 
   if (!file_put_contents($file, $contents)) {
-    print 'E404: unable to save file in ' . $file;
-    do_404();
+    do_404('unable to save file in ' . $file);
   }
 
-  // Reload page
+  chmod($file, $perms);
+
+  // Reload page.
   $location = $_SERVER['REQUEST_URI'];
 }
 
-// Redirect to real file
+// Redirect to real file.
 header('Location: ' . $location);
